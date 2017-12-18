cdd60fa5d211142a5701d112e174bb5f302efcfc
Maciej <zorteran@gmail.com>
2017-12-18 08:55:23 +0100

    added pages

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AboutPage extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>About</h1>
+                <p>This application uses React, Redux, React Router and variety of other helpful libraries.</p>
+            </div>
+        );
+    }
+
+}
