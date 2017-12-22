94eb4f386f83aec8b3ce51308b34dd0d8a9ad4f8
Maciej <zorteran@gmail.com>
2017-12-22 19:11:39 +0100

    changed action and reducer to load courses from mockApi

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import {loadCourses} from './actions/courseActions';
 import './styles/styles.css'; //webpack can import css files too
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());
 render(
     <Provider store={store}>
         <Router history={browserHistory} routes={routes} />
