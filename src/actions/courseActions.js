7be80e333b1d482bc1d53b96a0b37d9c90d7481c
Maciej <zorteran@gmail.com>
2017-12-22 22:20:45 +0100

    separated courselist and courselistrow from coursepage

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -11,6 +11,6 @@ export function loadCourses() {
             dispatch(loadCoursesSuccess(courses));
         }).catch(error => {
             throw(error);
-        })
-    }
+        });
+    };
 }
